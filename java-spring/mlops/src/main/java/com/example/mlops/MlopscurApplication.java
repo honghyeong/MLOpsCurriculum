392ba77345dab0b47c3896f86f9b2f77b60e7ee6
honghyeong <scarletgold0601@gmail.com>
2022-03-07 15:56:36 +0900

    build: create mlops api javaspring

---
+++ java-spring/mlops/src/main/java/com/example/mlops/MlopscurApplication.java
@@ -0,0 +1,13 @@
+package com.example.mlops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MlopscurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MlopscurApplication.class, args);
+	}
+
+}
---
+++ java-spring/mlops/src/test/java/com/example/mlops/MlopscurApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mlops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MlopscurApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
