af6eef023b457f6a51b57b8a49824f904bb907f0
honghyeong <scarletgold0601@gmail.com>
2022-03-07 16:17:52 +0900

    build: create project

---
+++ assignments/api-spring/mlopsapi/src/main/java/mlops/mlopsapi/MlopsapiApplication.java
@@ -0,0 +1,13 @@
+package mlops.mlopsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MlopsapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MlopsapiApplication.class, args);
+	}
+
+}
---
+++ assignments/api-spring/mlopsapi/src/test/java/mlops/mlopsapi/MlopsapiApplicationTests.java
@@ -0,0 +1,13 @@
+package mlops.mlopsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MlopsapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
