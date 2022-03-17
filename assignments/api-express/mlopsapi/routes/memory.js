d8d36d67d130b50af3da3529391cda46c8ebce59
honghyeong <scarletgold0601@gmail.com>
2022-03-17 15:28:14 +0900

    feat: docker complete

--- assignments/api-express/mlopsapi/routes/memory.js
+++ assignments/api-express/mlopsapi/routes/memory.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 var fs = require("fs");
 // require("dotenv").config();
-
+// const LOCAL_DB = "/../data/user.json";
 const { LOCAL_DB } = process.env;
 /* GET home page. */
 // router.get("/", function (req, res, next) {
