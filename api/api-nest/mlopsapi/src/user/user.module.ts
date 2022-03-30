5d5cea7a3257fd0aef820957148af298e77b3bd3
honghyeong <scarletgold0601@gmail.com>
2022-03-30 13:35:56 +0900

    feat: init user api

---
+++ api/api-nest/mlopsapi/src/user/user.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserController } from './user.controller';
+import { UserService } from './user.service';
+
+@Module({
+  controllers: [UserController],
+  providers: [UserService],
+})
+export class UserModule {}
