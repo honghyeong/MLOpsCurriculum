88da295ddbd33fa4d2b54ae1bd7936a4548f5cf2
honghyeong <scarletgold0601@gmail.com>
2022-04-01 09:27:19 +0900

    feat: e2e testing todo

---
+++ api/api-nest/mlopsapi/src/user/http-exception.filter.ts
@@ -0,0 +1,7 @@
+// import { ArgumentsHost, Catch, ExceptionFilter, HttpException } from "@nestjs/common";
+// import { QueryFailedError } from "typeorm";
+
+// @Catch(QueryFailedError)
+// export class QueryFailedExceptionFilter implements ExceptionFilter{
+//     catch(exception:QueryFailedError,host:ArgumentsHost)
+// }
