267c287f1360ae7e5d70beb3a35378ee010ea71c
honghyeong <scarletgold0601@gmail.com>
2022-04-12 13:08:25 +0900

    refactor: delete query optimization

--- api/api-nest/mlopsapi/src/main.ts
+++ api/api-nest/mlopsapi/src/main.ts
@@ -2,10 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cf from 'config';
 import { config } from 'dotenv';
+import { ValidationPipe } from '@nestjs/common';
 const port = cf.get('service').port;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // app.useGlobalPipes(new ValidationPipe());
   await app.listen(port);
 }
 bootstrap();
